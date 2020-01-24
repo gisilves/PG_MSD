d43e05bcbf051fc3b33e56208a0535f00aae6b06
gisilves <gianluigi.silvestre@pg.infn.it>
2020-01-24 11:00:23 +0100

    Analysis update

--- Root/src/miniTRB_clusterize.cxx
+++ Root/src/miniTRB_clusterize.cxx
@@ -1,228 +1,265 @@
-#include <iostream>
-#include "TFile.h"
-#include "TTree.h"
 #include "TChain.h"
+#include "TFile.h"
 #include "TH1.h"
 #include "TH2.h"
+#include "TTree.h"
 #include "omp.h"
+#include <iostream>
 
 #include "miniTRB.h"
 
 #define verbose false
-#define NChannels 384 
+#define NChannels 384
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 
-    //////////////////Histos
-    TH1D *hEnergyCluster = new TH1D("hEnergyCluster", "hEnergyCluster", 1000, 0, 500);
-    hEnergyCluster->GetXaxis()->SetTitle("ADC");
+  //////////////////Histos
+  TH1D *hEnergyCluster =
+      new TH1D("hEnergyCluster", "hEnergyCluster", 2000, 0, 1000);
+  hEnergyCluster->GetXaxis()->SetTitle("ADC");
 
-    TH1D *hEnergyClusterSeed = new TH1D("hEnergyClusterSeeed", "hEnergyClusterSeed", 1000, 0, 500);
-    hEnergyClusterSeed->GetXaxis()->SetTitle("ADC");
+  TH1D *hEnergyCluster1Strip =
+      new TH1D("hEnergyCluster1Strip", "hEnergyCluster1Strip", 2000, 0, 1000);
+  hEnergyCluster1Strip->GetXaxis()->SetTitle("ADC");
 
-    TH1D *hPercentageSeed = new TH1D("hPercentageSeeed", "hPercentageSeed", 200, 20, 100);
-    hPercentageSeed->GetXaxis()->SetTitle("percentage");
+  TH1D *hEnergyCluster2Strip =
+      new TH1D("hEnergyCluster2Strip", "hEnergyCluster2Strip", 2000, 0, 1000);
+  hEnergyCluster2Strip->GetXaxis()->SetTitle("ADC");
 
-    TH1D *hPercSeedintegral = new TH1D("hPercSeeedintegral", "hPercSeedintegral", 200, 20, 100);
-    hPercSeedintegral->GetXaxis()->SetTitle("percentage");
+  TH1D *hEnergyClusterManyStrip = new TH1D(
+      "hEnergyClusterManyStrip", "hEnergyClusterManyStrip", 2000, 0, 1000);
+  hEnergyClusterManyStrip->GetXaxis()->SetTitle("ADC");
 
-    TH1D *hClusterCharge = new TH1D("hClusterCharge", "hClusterCharge", 1000, -0.5, 5.5);
-    hClusterCharge->GetXaxis()->SetTitle("Charge");
+  TH1D *hEnergyClusterSeed =
+      new TH1D("hEnergyClusterSeed", "hEnergyClusterSeed", 2000, 0, 1000);
+  hEnergyClusterSeed->GetXaxis()->SetTitle("ADC");
 
-    TH1D *hSeedCharge = new TH1D("hSeedCharge", "hSeedCharge", 1000, -0.5, 5.5);
-    hSeedCharge->GetXaxis()->SetTitle("Charge");
+  TH1D *hPercentageSeed =
+      new TH1D("hPercentageSeed", "hPercentageSeed", 200, 20, 100);
+  hPercentageSeed->GetXaxis()->SetTitle("percentage");
 
-    TH1D *hClusterSN = new TH1D("hClusterSN", "hClusterSN", 200, 0, 100);
-    hClusterSN->GetXaxis()->SetTitle("S/N");
+  TH1D *hPercSeedintegral =
+      new TH1D("hPercSeedintegral", "hPercSeedintegral", 200, 20, 100);
+  hPercSeedintegral->GetXaxis()->SetTitle("percentage");
 
-    TH1D *hSeedSN = new TH1D("hSeedSN", "hSeedSN", 200, 0, 100);
-    hSeedSN->GetXaxis()->SetTitle("S/N");
+  TH1D *hClusterCharge =
+      new TH1D("hClusterCharge", "hClusterCharge", 1000, -0.5, 5.5);
+  hClusterCharge->GetXaxis()->SetTitle("Charge");
 
-    TH1D *hClusterCog = new TH1D("hClusterCog", "hClusterCog", 150, 0, NChannels);
-    hClusterCog->GetXaxis()->SetTitle("cog");
+  TH1D *hSeedCharge = new TH1D("hSeedCharge", "hSeedCharge", 1000, -0.5, 5.5);
+  hSeedCharge->GetXaxis()->SetTitle("Charge");
 
-    TH1D *hSeedPos = new TH1D("hSeedPos", "hSeedPos", 150, 0, NChannels);
-    hSeedPos->GetXaxis()->SetTitle("strip");
+  TH1D *hClusterSN = new TH1D("hClusterSN", "hClusterSN", 1000, 0, 500);
+  hClusterSN->GetXaxis()->SetTitle("S/N");
 
-    TH1F *hNclus = new TH1F("hclus", "hclus", 10, -0.5, 9.5);
-    hNclus->GetXaxis()->SetTitle("n clusters");
+  TH1D *hSeedSN = new TH1D("hSeedSN", "hSeedSN", 1000, 0, 500);
+  hSeedSN->GetXaxis()->SetTitle("S/N");
 
-    TH1F *hNstrip = new TH1F("hNstrip", "hNstrip", 10, -0.5, 9.5);
-    hNstrip->GetXaxis()->SetTitle("n strips");
+  TH1D *hClusterCog = new TH1D("hClusterCog", "hClusterCog", 150, 0, NChannels);
+  hClusterCog->GetXaxis()->SetTitle("cog");
 
-    TH1F *hEta = new TH1F("hEta", "hEta", 100, -1, 1);
-    hEta->GetXaxis()->SetTitle("Eta");
+  TH1D *hSeedPos = new TH1D("hSeedPos", "hSeedPos", 150, 0, NChannels);
+  hSeedPos->GetXaxis()->SetTitle("strip");
 
-    TH2F *hADCvsWidth = new TH2F("hADCvsWidth", "hADCvsWidth", 10, -0.5, 9.5, 1000, 0, 500);
-    hADCvsWidth->GetXaxis()->SetTitle("# of strips");
-    hADCvsWidth->GetYaxis()->SetTitle("ADC");
+  TH1F *hNclus = new TH1F("hclus", "hclus", 10, -0.5, 9.5);
+  hNclus->GetXaxis()->SetTitle("n clusters");
 
-    TH2F *hADCvsPos = new TH2F("hADCvsPos", "hADCvsPos", NChannels, 0, NChannels, 1000, 0, 500);
-    hADCvsPos->GetXaxis()->SetTitle("cog");
-    hADCvsPos->GetYaxis()->SetTitle("ADC");
+  TH1F *hNstrip = new TH1F("hNstrip", "hNstrip", 10, -0.5, 9.5);
+  hNstrip->GetXaxis()->SetTitle("n strips");
 
-    TH2F *hADCvsEta = new TH2F("hADCvsEta", "hADCvsEta", 200, -1, 1, 1000, 0, 500);
-    hADCvsEta->GetXaxis()->SetTitle("eta");
-    hADCvsEta->GetYaxis()->SetTitle("ADC");
+  TH1F *hNstripSeed = new TH1F("hNstripSeed", "hNstripSeed", 10, -0.5, 9.5);
+  hNstripSeed->GetXaxis()->SetTitle("n strips over seed threshold");
 
-    TH2F *hADCvsSN = new TH2F("hADCvsSN", "hADCvsSN", 500, 0, 50, 1000, 0, 500);
-    hADCvsSN->GetXaxis()->SetTitle("S/N");
-    hADCvsSN->GetYaxis()->SetTitle("ADC");
+  TH1F *hEta = new TH1F("hEta", "hEta", 100, -1, 1);
+  hEta->GetXaxis()->SetTitle("Eta");
 
-    TH2F *hNStripvsSN = new TH2F("hNstripvsSN", "hNstripvsSN", 1000, 0, 50, 5, -0.5, 4.5);
-    hNStripvsSN->GetXaxis()->SetTitle("S/N");
-    hNStripvsSN->GetYaxis()->SetTitle("# of strips");
+  TH1F *hDifference = new TH1F("hDifference", "hDifference", 200, -50, 50);
+  hDifference->GetXaxis()->SetTitle("Difference");
 
-    if (argc < 10)
-    {
-        std::cout << "Usage:\n ./miniTRB_clusterize <output_rootfile> <calibration file> <high threshold> <low threshold> <symmetric clusters> <symmetric clusters width> <use absolute thresholds> <common noise type> <first input root-filename> [second input root-filename] ..." << std::endl;
-        return 1;
+  TH2F *hADCvsWidth =
+      new TH2F("hADCvsWidth", "hADCvsWidth", 10, -0.5, 9.5, 2000, 0, 1000);
+  hADCvsWidth->GetXaxis()->SetTitle("# of strips");
+  hADCvsWidth->GetYaxis()->SetTitle("ADC");
+
+  TH2F *hADCvsPos = new TH2F("hADCvsPos", "hADCvsPos", NChannels, 0, NChannels,
+                             2000, 0, 1000);
+  hADCvsPos->GetXaxis()->SetTitle("cog");
+  hADCvsPos->GetYaxis()->SetTitle("ADC");
+
+  TH2F *hADCvsEta =
+      new TH2F("hADCvsEta", "hADCvsEta", 200, -1, 1, 2000, 0, 1000);
+  hADCvsEta->GetXaxis()->SetTitle("eta");
+  hADCvsEta->GetYaxis()->SetTitle("ADC");
+
+  TH2F *hADCvsSN = new TH2F("hADCvsSN", "hADCvsSN", 500, 0, 50, 2000, 0, 1000);
+  hADCvsSN->GetXaxis()->SetTitle("S/N");
+  hADCvsSN->GetYaxis()->SetTitle("ADC");
+
+  TH2F *hNStripvsSN =
+      new TH2F("hNstripvsSN", "hNstripvsSN", 1000, 0, 50, 5, -0.5, 4.5);
+  hNStripvsSN->GetXaxis()->SetTitle("S/N");
+  hNStripvsSN->GetYaxis()->SetTitle("# of strips");
+
+  if (argc < 10) {
+    std::cout
+        << "Usage:\n ./miniTRB_clusterize <output_rootfile> <calibration file> "
+           "<high threshold> <low threshold> <symmetric clusters> <symmetric "
+           "clusters width> <use absolute thresholds> <common noise type> "
+           "<first input root-filename> [second input root-filename] ..."
+        << std::endl;
+    return 1;
+  }
+
+  // Join ROOTfiles in a single chain
+  TChain *chain = new TChain("raw_events");
+  for (int ii = 9; ii < argc; ii++) {
+    std::cout << "Adding file " << argv[ii] << " to the chain..." << std::endl;
+    chain->Add(argv[ii]);
+  }
+
+  Long64_t entries = chain->GetEntries();
+  // int entries = 2;
+  printf("This run has %lld entries\n", entries);
+
+  // Read raw event from input chain TTree
+  std::vector<unsigned short> *raw_event = 0;
+  TBranch *RAW = 0;
+  chain->SetBranchAddress("RAW Event", &raw_event, &RAW);
+
+  // Create output ROOTfile
+  TString output_filename = argv[1];
+  TFile *foutput = new TFile(output_filename.Data(), "RECREATE");
+  foutput->cd();
+
+  calib cal;
+  read_calib(argv[2], &cal);
+
+  int perc = 0;
+
+  // Loop over events
+  for (int index_event = 0; index_event < entries; index_event++) {
+    chain->GetEntry(index_event);
+
+    Double_t pperc = 10.0 * ((index_event + 1.0) / entries);
+    if (pperc >= perc) {
+      std::cout << "Processed " << (index_event + 1) << " out of " << entries
+                << ":" << (int)(100.0 * (index_event + 1.0) / entries) << "%"
+                << std::endl;
+      perc++;
     }
 
-    //Join ROOTfiles in a single chain
-    TChain *chain = new TChain("raw_events");
-    for (int ii = 9; ii < argc; ii++)
-    {
-        std::cout << "Adding file " << argv[ii] << " to the chain..." << std::endl;
-        chain->Add(argv[ii]);
+    std::vector<float> signal;
+
+    if (raw_event->size() == 384 || raw_event->size() == 640) {
+      if (cal.ped.size() >= raw_event->size()) {
+        for (size_t i = 0; i != raw_event->size(); i++) {
+          if (cal.status[i] == 0) {
+            signal.push_back(raw_event->at(i) - cal.ped[i]);
+          } else {
+            signal.push_back(0);
+          }
+        }
+      } else {
+        if (verbose) {
+          std::cout << "Error: calibration file is not compatible" << std::endl;
+        }
+      }
     }
 
-    Long64_t entries = chain->GetEntries();
-    //int entries = 2;
-    printf("This run has %lld entries\n", entries);
-
-    //Read raw event from input chain TTree
-    std::vector<unsigned short> *raw_event = 0;
-    TBranch *RAW = 0;
-    chain->SetBranchAddress("RAW Event", &raw_event, &RAW);
-
-    //Create output ROOTfile
-    TString output_filename = argv[1];
-    TFile *foutput = new TFile(output_filename.Data(), "RECREATE");
-    foutput->cd();
-
-    calib cal;
-    read_calib(argv[2], &cal);
-
-    int perc = 0;
-
-    //Loop over events
-    for (int index_event = 0; index_event < entries; index_event++)
-    {
-        chain->GetEntry(index_event);
-
-        Double_t pperc = 10.0 * ((index_event + 1.0) / entries);
-        if (pperc >= perc)
-        {
-            std::cout << "Processed " << (index_event + 1) << " out of " << entries << ":" << (int)(100.0 * (index_event + 1.0) / entries) << "%" << std::endl;
-            perc++;
-        }
-
-        std::vector<float> signal;
-
-        if (raw_event->size() == 384 || raw_event->size() == 640)
-        {
-            if (cal.ped.size() >= raw_event->size())
-            {
-                for (size_t i = 0; i != raw_event->size(); i++)
-                {
-                    if (cal.status[i] == 0)
-                    {
-                        signal.push_back(raw_event->at(i) - cal.ped[i]);
-                    }
-                    else
-                    {
-                        signal.push_back(0);
-                    }
-                }
-            }
-            else
-            {
-                if (verbose)
-                {
-                    std::cout << "Error: calibration file is not compatible" << std::endl;
-                }
-            }
-        }
-
-        std::vector<float> signal2(signal.size());
+    std::vector<float> signal2(signal.size());
 
 #pragma omp parallel for
-        for (size_t i = 0; i < signal.size(); i++)
-        {
-            if (GetCN(&signal, i / 64, atoi(argv[8])))
-            {
-                signal2.at(i) = signal.at(i) - GetCN(&signal, i / 64, atoi(argv[8]));
-            }
-            else
-            {
-                signal2.at(i) = 0;
-            }
-        }
-
-        try
-        {
-            std::vector<cluster> result = clusterize(&cal, &signal2, atof(argv[3]), atof(argv[4]), atoi(argv[5]), atoi(argv[6]), atoi(argv[7]));
-            for (int i = 0; i < result.size(); i++)
-            {
-                if (i == 0)
-                {
-                    hNclus->Fill(result.size());
-                }
-                hEnergyCluster->Fill(GetClusterSignal(result.at(i)));
-                hEnergyClusterSeed->Fill(GetClusterSeedADC(result.at(i)));
-                hClusterCharge->Fill(GetClusterMIPCharge(result.at(i)));
-                hSeedCharge->Fill(GetSeedMIPCharge(result.at(i)));
-                hPercentageSeed->Fill(100 * GetClusterSeedADC(result.at(i)) / GetClusterSignal(result.at(i)));
-                hClusterSN->Fill(GetClusterSN(result.at(i), &cal));
-                hSeedSN->Fill(GetSeedSN(result.at(i), &cal));
-                hClusterCog->Fill(GetClusterCOG(result.at(i)));
-                hSeedPos->Fill(GetClusterSeed(result.at(i)));
-                hNstrip->Fill(GetClusterWidth(result.at(i)));
-                hEta->Fill(GetClusterEta(result.at(i)));
-                hADCvsWidth->Fill(GetClusterWidth(result.at(i)), GetClusterSignal(result.at(i)));
-                hADCvsPos->Fill(GetClusterCOG(result.at(i)), GetClusterSignal(result.at(i)));
-                hADCvsEta->Fill(GetClusterEta(result.at(i)), GetClusterSignal(result.at(i)));
-                hADCvsSN->Fill(GetClusterSN(result.at(i), &cal), GetClusterSignal(result.at(i)));
-                hNStripvsSN->Fill(GetClusterSN(result.at(i), &cal), GetClusterWidth(result.at(i)));
-            }
-        }
-        catch (const char *msg)
-        {
-            if (verbose)
-            {
-                std::cerr << msg << "Skipping event " << index_event << std::endl;
-            }
-            continue;
-        }
+    for (size_t i = 0; i < signal.size(); i++) {
+      if (GetCN(&signal, i / 64, atoi(argv[8]))) {
+        signal2.at(i) = signal.at(i) - GetCN(&signal, i / 64, atoi(argv[8]));
+      } else {
+        signal2.at(i) = 0;
+      }
     }
 
-    hNclus->Write();
-    hEnergyCluster->Write();
-    hEnergyClusterSeed->Write();
-    hClusterCharge->Write();
-    hSeedCharge->Write();
-    hClusterSN->Write();
-    hSeedSN->Write();
-    hClusterCog->Write();
-    hSeedPos->Write();
-    hNstrip->Write();
-    hEta->Write();
-    hEtaRaw->Write();
-    hADCvsWidth->Write();
-    hADCvsPos->Write();
-    hADCvsEta->Write();
-    hADCvsSN->Write();
-    hNStripvsSN->Write();
+    try {
+      std::vector<cluster> result =
+          clusterize(&cal, &signal2, atof(argv[3]), atof(argv[4]),
+                     atoi(argv[5]), atoi(argv[6]), atoi(argv[7]));
+      for (int i = 0; i < result.size(); i++) {
 
-    Float_t sum = 0;
-    for (Int_t i = 1; i <= 200; i++)
-    {
-        sum += hPercentageSeed->GetBinContent(i);
-        hPercSeedintegral->SetBinContent(i, sum);
+        if (i == 0) {
+          hNclus->Fill(result.size());
+        }
+        hEnergyCluster->Fill(GetClusterSignal(result.at(i)));
+        if (result.at(i).width == 1) {
+          hEnergyCluster1Strip->Fill(GetClusterSignal(result.at(i)));
+        } else if (result.at(i).width == 2) {
+          hEnergyCluster2Strip->Fill(GetClusterSignal(result.at(i)));
+
+        } else {
+          hEnergyClusterManyStrip->Fill(GetClusterSignal(result.at(i)));
+        }
+
+        hEnergyClusterSeed->Fill(GetClusterSeedADC(result.at(i)));
+        hClusterCharge->Fill(GetClusterMIPCharge(result.at(i)));
+        hSeedCharge->Fill(GetSeedMIPCharge(result.at(i)));
+        hPercentageSeed->Fill(100 * GetClusterSeedADC(result.at(i)) /
+                              GetClusterSignal(result.at(i)));
+        hClusterSN->Fill(GetClusterSN(result.at(i), &cal));
+        hSeedSN->Fill(GetSeedSN(result.at(i), &cal));
+        hClusterCog->Fill(GetClusterCOG(result.at(i)));
+        hSeedPos->Fill(GetClusterSeed(result.at(i)));
+        hNstrip->Fill(GetClusterWidth(result.at(i)));
+        hEta->Fill(GetClusterEta(result.at(i)));
+        hADCvsWidth->Fill(GetClusterWidth(result.at(i)),
+                          GetClusterSignal(result.at(i)));
+        hADCvsPos->Fill(GetClusterCOG(result.at(i)),
+                        GetClusterSignal(result.at(i)));
+        hADCvsEta->Fill(GetClusterEta(result.at(i)),
+                        GetClusterSignal(result.at(i)));
+        hADCvsSN->Fill(GetClusterSN(result.at(i), &cal),
+                       GetClusterSignal(result.at(i)));
+        hNStripvsSN->Fill(GetClusterSN(result.at(i), &cal),
+                          GetClusterWidth(result.at(i)));
+        hNstripSeed->Fill(result.at(i).over);
+        if (result.at(i).width == 2) {
+          hDifference->Fill(result.at(i).ADC.at(0) - result.at(i).ADC.at(1));
+        }
+      }
+    } catch (const char *msg) {
+      if (verbose) {
+        std::cerr << msg << "Skipping event " << index_event << std::endl;
+      }
+      continue;
     }
+  }
 
-    foutput->Close();
-    return 0;
+  hNclus->Write();
+  hEnergyCluster->Write();
+  hEnergyCluster1Strip->Write();
+  hEnergyCluster2Strip->Write();
+  hEnergyClusterManyStrip->Write();
+  hEnergyClusterSeed->Write();
+  hClusterCharge->Write();
+  hSeedCharge->Write();
+  hClusterSN->Write();
+  hSeedSN->Write();
+  hClusterCog->Write();
+  hSeedPos->Write();
+  hNstrip->Write();
+  hNstripSeed->Write();
+  hEta->Write();
+  hADCvsWidth->Write();
+  hADCvsPos->Write();
+  hADCvsEta->Write();
+  hADCvsSN->Write();
+  hNStripvsSN->Write();
+  hDifference->Write();
+  //   Float_t sum = 0;
+  //   for (Int_t i = 1; i <= 200; i++) {
+  //     sum += hPercentageSeed->GetBinContent(i);
+  //     hPercSeedintegral->SetBinContent(i, sum);
+  //   }
+
+  //   hPercentageSeed->Write();
+  //   hPercSeedintegral->Write();
+
+  foutput->Close();
+  return 0;
 }
--- Root/src/miniTRB_compress.cxx
+++ Root/src/miniTRB_compress.cxx
@@ -1,193 +1,9 @@
 #include <iostream>
-#include <fstream>
-#include <cstring>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <iterator>
-#include "TROOT.h"
 #include "TFile.h"
 #include "TTree.h"
 #include "TString.h"
 
-int seek_endianess(std::fstream &file)
-{
-    bool found = false;
-    bool little_endianess;
-    unsigned char buffer[2];
-    unsigned short val;
-
-    file.seekg(0);
-
-    while (!found && !file.eof())
-    {
-        file.read(reinterpret_cast<char *>(&buffer), 2);
-        val = buffer[0] | (buffer[1] << 8);
-
-        if (val == 0xbbaa)
-        {
-            little_endianess = false;
-            found = true;
-        }
-        else if (val == 0xaabb)
-        {
-            little_endianess = true;
-            found = true;
-        }
-    }
-
-    if (!found)
-    {
-        std::cout << "Can't find endianess WORD in the file" << std::endl;
-        return -999;
-    }
-    else
-    {
-        return little_endianess;
-    }
-}
-
-int seek_header(std::fstream &file, bool little_endian)
-{
-    int offset = 0;
-    unsigned short header;
-    bool found = false;
-
-    unsigned char buffer[2];
-    unsigned short val;
-
-    if (little_endian)
-    {
-        header = 0xeb90;
-    }
-    else
-    {
-        header = 0x90eb;
-    }
-
-    while (!found && !file.eof())
-    {
-        file.seekg(offset);
-        file.read(reinterpret_cast<char *>(&buffer), 2);
-        val = buffer[0] | (buffer[1] << 8);
-
-        if (val == header)
-        {
-            found = true;
-        }
-        else
-        {
-            offset += 2;
-        }
-    }
-
-    if (!found)
-    {
-        std::cout << "Can't find event header in the file" << std::endl;
-        return -999;
-    }
-    else
-    {
-        return offset;
-    }
-}
-
-int seek_raw(std::fstream &file, int offset, bool little_endian)
-{
-    bool found = false;
-    bool is_raw = 0;
-    unsigned char buffer[2];
-    unsigned short val;
-
-    file.seekg(offset + 2);
-    file.read(reinterpret_cast<char *>(&buffer), 2);
-
-    if (little_endian)
-    {
-        val = (int)buffer[1];
-    }
-    else
-    {
-        val = (int)buffer[0];
-    }
-
-    if (val == 0xa0)
-    {
-        is_raw = true;
-        found = true;
-    }
-
-    return is_raw;
-}
-
-int seek_version(std::fstream &file)
-{
-    bool found = false;
-    int version;
-    unsigned char buffer[2];
-    unsigned short val;
-
-    file.seekg(0);
-
-    while (!found && !file.eof())
-    {
-        file.read(reinterpret_cast<char *>(&buffer), 2);
-        val = buffer[0] | (buffer[1] << 8);
-
-        if (val == 0x1212 || val == 0x1313)
-        {
-            version = val;
-            found = true;
-        }
-    }
-
-    if (!found)
-    {
-        std::cout << "Can't find version WORD in the file" << std::endl;
-        return -999;
-    }
-    else
-    {
-        return version;
-    }
-}
-
-std::vector<unsigned short> read_event(std::fstream &file, int offset, int version, int evt)
-{
-    file.seekg(offset + 4 + evt * 1024);
-
-    int event_size;
-
-    if (version == 0x1212)
-    {
-        event_size = 384;
-    }
-    else if (version == 0x1313)
-    {
-        event_size = 640;
-    }
-    else
-    {
-        std::cout << "Error: unknown miniTRB version" << std::endl;
-    }
-
-    std::vector<unsigned short> buffer(event_size);
-    file.read(reinterpret_cast<char *>(buffer.data()), buffer.size() * 2);
-    std::streamsize s = file.gcount();
-    buffer.resize(s / 2);
-
-    std::vector<unsigned short> event(s / 2);
-
-    int j = 0;
-    for (int i = 0; i < buffer.size(); i += 2)
-    {
-        event.at(j) = buffer.at(i);
-        event.at(j + buffer.size() / 2) = buffer.at(i + 1);
-        j++;
-    }
-
-    return event;
-}
+#include "miniTRB.h"
 
 int main(int argc, char *argv[])
 {
@@ -228,6 +44,10 @@ int main(int argc, char *argv[])
     else if (version == 0x1313)
     {
         std::cout << "File from 10VA miniTRB" << std::endl;
+    }
+    else
+    {
+        return 1;
     }
 
     //Find file endianess. TODO: implement Big Endian bytes swapping
