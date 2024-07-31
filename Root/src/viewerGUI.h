f84dbc2263d38159c9e19b46d0f9eb41380cc3d8
gisilves <gianluigi.silvestre@gmail.com>
2024-07-31 11:43:35 +0200

    Removed non working OM from raw_viewer and fixed different detectors data loading from file

--- Root/src/viewerGUI.h
+++ Root/src/viewerGUI.h
@@ -4,7 +4,6 @@
 #include <TQObject.h>
 #include <RQ_OBJECT.h>
 #include "TGraph.h"
-#include "udpSocket.h"
 #include "event.h"
 #include <thread>
 
@@ -31,12 +30,12 @@ private:
   TGMainFrame *fMain;
   TRootEmbeddedCanvas *fEcanvas;
   TGNumberEntry *fNumber, *fNumber1, *fNumber2, *fNumber3;
-  TGHorizontalFrame *fHor_Buttons, *fHor_OM_Buttons;
-  TGHorizontalFrame *fHor_Numbers, *fHor_Numbers_OM;
-  TGHorizontalFrame *fHor_Pedestal, *fHor_Pedestal_OM;
-  TGHorizontalFrame *fHor_Status, *fHor_Status_OM;
+  TGHorizontalFrame *fHor_Buttons;
+  TGHorizontalFrame *fHor_Numbers;
+  TGHorizontalFrame *fHor_Pedestal;
+  TGHorizontalFrame *fHor_Status;
   TGHorizontalFrame *fHor_Files;
-  TGTextButton *fExit, *fExit2, *fDraw, *fOpen, *fSave, *fOpenCalib, *fStart, *fStop;
+  TGTextButton *fExit, *fExit2, *fDraw, *fOpen, *fSave, *fOpenCalib;
   TGLabel *evtLabel, *fileLabel, *pedLabel, *detectorLabel, *detectorLabel2, *boardsLabel;
   TGLabel *calibLabel;
   TGTextView *fStatusBar, *fStatusBar2;
@@ -49,25 +48,12 @@ private:
 
   std::vector<calib> calib_data;
 
-  // UDP server to on-line monitor
-  std::string kUdpAddr = "localhost"; //!< UDP Server address (x.x.x.x format)
-  int kUdpPort = 8890;                //!< UDP server port
-  udpServer *omServer;
-
-  std::thread th1;
-  bool running = false;
-
 public:
   MyMainFrame(const TGWindow *p, UInt_t w, UInt_t h);
   virtual ~MyMainFrame();
   void DoDraw();
-  void DoDrawOM(int evtnum, int detector, char calibfile[200], std::vector<uint32_t> evt);
   void DoOpen();
   void DoClose();
-  void DoStart();
-  void DoStop();
-  void DoGetUDP();
-  void JobThread();
   void DoOpenCalib();
   void DoOpenCalibOnly();
   void viewer(int evt, int detector, char filename[200], char calibfile[200], int boards);
