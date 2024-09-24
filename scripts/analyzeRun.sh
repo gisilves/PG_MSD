7f832459307570607a921df0db7c4f3a93a18f99
evila <emanuele.villa@cern.ch>
2024-09-24 15:24:57 +0200

    removed useless bits of script

--- scripts/analyzeRun.sh
+++ scripts/analyzeRun.sh
@@ -8,18 +8,19 @@ outputDirectory=$REPO_HOME"/output/"
 
 # Initialize variables
 settingsFile=""
+fileName=""
 noCompile=false
 cleanCompile=false
 
 # Function to print help message
 print_help() {
     echo "*****************************************************************************"
-    echo "Usage: ./createOutputTree.sh -f <first_run> [-l <last_run>] -s <settings_file> [-h]"
+    echo "Usage: ./createOutputTree.sh -f <first_run> [-l <last_run>] -j <settings_file> [-h]"
     echo "  -p | --home-path      path to the code home directory (/your/path/tof-reco, no / at the end). There is no default."
     echo "  -r | --run-name       full run name, with ot without .dat"
     echo "  -f | --first-run      first run number"
     echo "  -l | --last-run       last run number" 
-    echo "  -s | --settings-file  json settings file to run this app"
+    echo "  -j | --json-settings  json settings file to run this app"
     echo "  --no-compile          do not recompile the code. Default is to recompile the code"
     echo "  --clean-compile       clean and recompile the code. Default is to recompile the code without cleaning"
     echo "  --source-lxplus       source the lxplus environment. Default is to not source the lxplus environment"
@@ -47,7 +48,7 @@ while [[ $# -gt 0 ]]; do
         lastRun="$2"
         shift 2
         ;;
-      -s|--settings-file)
+      -j|--json-settings)
         settingsFile="$2"
         shift 2
         ;;
@@ -74,7 +75,7 @@ done
 
 if [ -z "$settingsFile" ]
 then
-  echo "Please specify the settings file, using flag -s <settings_file>. Stopping execution."
+  echo "Please specify the settings file, using flag -j <settings_file>. Stopping execution."
   exit 0
 fi
 
@@ -93,6 +94,9 @@ cd $REPO_HOME
 inputDirectory=$(awk -F'"' '/inputDirectory/{print $4}' "$settingsFile")
 outputDirectory=$(awk -F'"' '/outputDirectory/{print $4}' "$settingsFile")
 verbose=$(awk -F'"' '/verboseMode/{print $4}' "$settingsFile")
+debug=$(awk -F'"' '/debugMode/{print $4}' "$settingsFile")
+nsigma=$(awk -F'"' '/nSigma/{print $4}' "$settingsFile") # TODO add fallback if not defined
+showPlots=$(awk -F'"' '/showPlots/{print $4}' "$settingsFile") # TODO add fallback if not defined
 
 # Check if the user has selected a run name or number(s)
 if [ -z "$fileName" ] 
@@ -102,7 +106,7 @@ then
     echo "Please select a run number (or an interval) from command line to convert the data into ROOT format. 
     You can select only the first run, or both the first and the last run.
     Usage: 
-    $ ./createOutputTree.sh -f <first_run> [-l <last_run>] -s <settings_file> [-h]"
+    $ ./analyzeRuns.sh -f <first_run> [-l <last_run>] -j <json_settings_file> [-h]"
     exit 0
     fi
     if [ -z "$lastRun" ]
@@ -144,44 +148,17 @@ cd $REPO_HOME/build # executable is here, for now
 
 if [ -n "$fileName" ]
 then
-  
-  # only convert if the file is not already in root format
-  if [ -f "${outputDirectory}/${fileName}.root" ]
-  then
-    echo "File ${outputDirectory}/${fileName}.root already exists. Skipping conversion."
-  else
-    convert_data="./PAPERO_convert ${inputDirectory}/${fileName}.dat ${outputDirectory}/${fileName}.root --dune"
-    echo "Executing command: "$convert_data
-    $convert_data
-  fi
-
-  # only extract calibration if the file does not exist already
-  if [ -f "${outputDirectory}/${fileName}.cal" ]
-  then
-    echo "File ${outputDirectory}/${fileName}.cal already exists. Skipping calibration extraction."
-  else
-    extract_calibration="./calibration ${outputDirectory}/${fileName}.root --output ${outputDirectory}/${fileName} --dune --fast"
-    echo "Executing command: "$extract_calibration
-    $extract_calibration
-  fi
-
-  analyze_data="./dataAnalyzer -r ${outputDirectory}/${fileName}.root -c ${outputDirectory}/${fileName}.cal -o ${outputDirectory}"
-  # if verbose is true, append -v
-  if [ "$verbose" = true ]
-  then
-    echo "Verbose mode is on."
-    analyze_data+=" -v"
-  fi
-  echo "Executing command: "$analyze_data
-  $analyze_data
-  # echo "Analyzed data saved in ${outputDirectory}/${fileName}.root"
-
-  exit 0
+  # just to get in the loop below
+  firstRun=0
+  lastRun=0
 fi
 
 # Iterate over all the selected runs
 for ((runit = $firstRun; runit <= $lastRun; runit++ ))
 do
+  if [ -z $fileName ]
+  then
+    echo "Looking for files by run number"
     echo "runit: $runit"
     runit_5d=$(printf "%05d" "$runit")
     filePath=$(find "$inputDirectory" -name "SCD_RUN${runit_5d}_*.dat")
@@ -198,6 +175,7 @@ do
 
     echo "Dealing with file: "$filePath
     echo "outputDirectory: "$outputDirectory
+  
 
     # in case it doesn't exist, creating output directory
     mkdir -p $outputDirectory
@@ -208,37 +186,52 @@ do
     echo ""
 
     fileName=$(basename $filePath)
-    filename=${filename%.*}
+    fileName=${fileName%.*}
     echo "File name is: "$fileName
+  else
+    echo "Received a single file path ${fileName}"
+    filePath="${inputDirectory}/${fileName}.dat"
+  fi
 
-    if [ -f "${outputDirectory}/${fileName}.root" ]
-    then
-        echo "File ${outputDirectory}/${fileName}.root already exists. Skipping conversion."
-    else
-        convert_data="./PAPERO_convert ${filePath} ${outputDirectory}/${fileName}.root --dune"
-        echo "Executing command: "$convert_data
-        $convert_data
-    fi
+  if [ -f "${outputDirectory}/${fileName}.root" ]
+  then
+      echo "File ${outputDirectory}/${fileName}.root already exists. Skipping conversion."
+  else
+      convert_data="./PAPERO_convert ${filePath} ${outputDirectory}/${fileName}.root --dune"
+      echo "Executing command: "$convert_data
+      $convert_data
+  fi
 
-    if [ -f "${outputDirectory}/${fileName}.cal" ]
-    then
-        echo "File ${outputDirectory}/${fileName}.cal already exists. Skipping calibration extraction."
-    else
-        extract_calibration="./calibration ${outputDirectory}/${fileName}.root --output ${outputDirectory}/${fileName} --dune --fast"
-        echo "Executing command: "$extract_calibration
-        $extract_calibration
-    fi
+  if [ -f "${outputDirectory}/${fileName}.cal" ]
+  then
+      echo "File ${outputDirectory}/${fileName}.cal already exists. Skipping calibration extraction."
+  else
+      extract_calibration="./calibration ${outputDirectory}/${fileName}.root --output ${outputDirectory}/${fileName} --dune --fast"
+      echo "Executing command: "$extract_calibration
+      $extract_calibration
+  fi
 
-    analyze_data="./dataAnalyzer -r ${outputDirectory}/${fileName}.root -c ${outputDirectory}/${fileName}.cal -o ${outputDirectory}"
-    # if verbose is true, append -v
-    if [ "$verbose" = true ]
-    then
-        echo "Verbose mode is on."
-        analyze_data+=" -v"
-    fi
-    echo "Executing command: "$analyze_data
-    $analyze_data
+  analyze_data="./dataAnalyzer -r ${outputDirectory}/${fileName}.root -c ${outputDirectory}/${fileName}.cal -o ${outputDirectory} -s ${nsigma}"
+  
+  if [ "$verbose" = true ]
+  then
+      echo "Verbose mode is on."
+      analyze_data+=" -v"
+  fi
 
+  if [ "$debug" = true ]
+  then
+      echo "Debug mode is on."
+      analyze_data+=" -d"
+  fi
+
+  if [ "$showPlots" = true ]
+  then
+      analyze_data+=" --show-plots"
+  fi
+
+  echo "Executing command: "$analyze_data
+  $analyze_data
  
 done
 
